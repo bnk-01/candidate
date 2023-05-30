7ae02c47fa0c0475844c15a67d46b9e5aecb8845
bnk <samuel.egheghe@yahoo.com>
2023-05-30 12:16:36 +0200

    added docstring to blog views.py

--- blog/views.py
+++ blog/views.py
@@ -5,19 +5,52 @@ from django.contrib.auth.decorators import login_required
 
 
 class PostList(generic.ListView):
+    """
+        A view displaying a list of blog posts.
+
+        Attributes:
+            queryset (QuerySet): The queryset used to retrieve the blog posts.
+            template_name (str): The name of the template used for rendering the view.
+        """
+
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
 
 
 class PostDetail(generic.DetailView):
+    """
+       A view displaying a single blog post.
+
+       Attributes:
+           model (Model): The model class representing the blog post.
+           template_name (str): The name of the template used for rendering the view.
+       """
     model = Post
     template_name = 'post_detail.html'
 
 
 def home(request):
+    """
+      Renders the home page.
+
+      Parameters:
+          request (HttpRequest): The request object.
+
+      Returns:
+          HttpResponse: The rendered response.
+      """
     return render(request, "home.html")
 
 
 @login_required
 def about(request):
+    """
+       Renders the about page, accessible only to authenticated users.
+
+       Parameters:
+           request (HttpRequest): The request object.
+
+       Returns:
+           HttpResponse: The rendered response.
+       """
     return render(request, "about.html")
