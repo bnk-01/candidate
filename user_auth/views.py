dac6b788b03b343c1bf568d5c64245ea9d945e4b
bnk <samuel.egheghe@yahoo.com>
2023-05-30 12:17:46 +0200

    added docstring to user_auth views.py

--- user_auth/views.py
+++ user_auth/views.py
@@ -6,6 +6,20 @@ from django.contrib.auth.forms import AuthenticationForm
 
 
 def register_request(request):
+    """
+       This function handles the user registration request.
+
+       If the request method is POST, it validates the registration form.
+       If the form is valid, it creates a new user, logs them in, and redirects them to the login page.
+       If the form is invalid, it displays an error message.
+       If the request method is GET, it renders the registration page.
+
+       Parameters:
+           request (HttpRequest): The request object.
+
+       Returns:
+           HttpResponse: The rendered response.
+       """
     if request.method == "POST":
         form = NewUserForm(request.POST)
         if form.is_valid():
@@ -19,6 +33,20 @@ def register_request(request):
 
 
 def login_request(request):
+    """
+        This function handles the user login request.
+
+        If the request method is POST, it validates the login form.
+        If the form is valid, it authenticates the user, logs them in, and redirects them to the index page.
+        If the form is invalid, it displays an error message.
+        If the request method is GET, it renders the login page.
+
+        Parameters:
+            request (HttpRequest): The request object.
+
+        Returns:
+            HttpResponse: The rendered response.
+        """
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
@@ -38,6 +66,17 @@ def login_request(request):
 
 
 def logout_request(request):
+    """
+        This function handles the user logout request.
+
+        Logs out the user and redirects them to the home page.
+
+        Parameters:
+            request (HttpRequest): The request object.
+
+        Returns:
+            HttpResponse: The rendered response.
+        """
     logout(request)
     messages.info(request, "You have successfully logged out.")
     return redirect("home")
