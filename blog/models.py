be3795823b50e911ba652ea91a91a44753642036
bnk <samuel.egheghe@yahoo.com>
2023-05-30 12:14:30 +0200

    added docstring to blog models.py

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,24 @@ STATUS = (
 
 
 class Post(models.Model):
+    """
+        Blog post Model.
+
+        Attributes:
+            title (str): The title of the blog post.
+            slug (str): A slug field for the URL.
+            author (User): The author of the blog post.
+            updated_on (datetime): The date and time of the last update.
+            content (str): The content of the blog post.
+            created_on (datetime): The date and time of creation.
+            status (int): The status of the blog post.
+
+        Meta:
+            This orders post by creation date.
+
+        Methods:
+            Returns the title of the blog post as a string.
+        """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
@@ -20,4 +38,8 @@ class Post(models.Model):
         ordering = ['-created_on']
 
     def __str__(self):
+        """
+            Returns the title of the blog post as a string.
+
+         """
         return self.title
